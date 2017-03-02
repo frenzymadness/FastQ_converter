0496c28d8e9473f03400a68a0c7509254901bddf
Lumir Balhar <lbalhar@redhat.com>
2017-03-02 14:29:08 +0100

    Add tests

---
+++ test_FastQ_converter.py
@@ -0,0 +1,20 @@
+from FastQ_converter import convert_to_csv
+from io import StringIO
+
+in1 = StringIO('@idXXX\nsequenceXXXX\n+\nqualityXXXX\n')
+out1 = StringIO('"SeqID","sequence","quality"\n"@idXXX","sequenceXXXX","qualityXXXX"\n')
+
+in2 = StringIO('X\nY\n+\nZ')
+out2 = StringIO('"SeqID","sequence","quality"\n')
+
+
+def test_convert_to_csv():
+    result = StringIO()
+    convert_to_csv(in1, result)
+    assert result.getvalue() == out1.getvalue()
+
+
+def test_empty_convert_to_csv():
+    result = StringIO()
+    convert_to_csv(in2, result)
+    assert result.getvalue() == out2.getvalue()
